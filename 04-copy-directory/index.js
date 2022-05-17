ca642911fafdc899ed50e3b0a5f25937c910573e
MaxKnyazev <max.knyazev77@gmail.com>
2022-05-17 20:00:50 +0300

    feat: complete task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,16 @@
+const path = require('path');
+const { readdir, mkdir, copyFile, rmdir } = require('fs/promises');
 
+const pathToFolder = path.join(__dirname, 'files');
+const pathToCopyFolder = path.join(__dirname, 'files-copy');
+
+(async () => {
+  await rmdir(pathToCopyFolder, {recursive: true});
+  await mkdir(pathToCopyFolder);
+  const files = await readdir(pathToFolder, {encoding: 'utf8', withFileTypes: true});
+  for (const file of files) {
+    const pathToFile = path.join(pathToFolder, file.name);
+    const pathToCopyFile = path.join(pathToCopyFolder, file.name);
+    await copyFile(pathToFile, pathToCopyFile);
+  }
+})().catch(err => console.error(err));
